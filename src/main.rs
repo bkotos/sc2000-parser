85746edba5bddacd4553664688649bbdd370d446
Brian Kotos <brian.kotos@gmail.com>
2019-09-28 17:45:16 -0400

    Wip

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,43 @@
 use std::fs;
 use nom::AsChar;
-//use nom::bytes::complete::{tag, take};
 use nom::number::complete::{be_u8,be_u64};
 use nom::number::Endianness;
 use nom::{IResult,Err,Needed,named,do_parse,tag,take,u32};
 
 pub struct Header {
-    file_size1: u32,
+    total_bytes: u32,
+    file_type: [char; 4] // TODO convert char array to string
 }
 
 named!(parse_sc2000(&[u8]) -> Header,
     do_parse!(
         tag!("FORM") >>
-//        file_size1: take!(4) >>
-        file_size1: u32!(Endianness::Big) >>
+        total_bytes: u32!(Endianness::Big) >>
+        file_type: take!(4) >>
         (
             Header {
-//                file_size1: file_size1[3]
-                file_size1: file_size1
+                total_bytes: total_bytes,
+                file_type: [file_type[0] as char, file_type[1] as char, file_type[2] as char, file_type[3] as char]
             }
         )
     )
 );
 
 fn main() {
-    let data = fs::read("/home/bkotos/Projects/sc2k-parser/assets/sbend.sc2").expect("Unable to read file");
+    // TODO dynamically pull file location
+    let bin: Vec<u8> = fs::read("/home/bkotos/Projects/sc2k-parser/assets/sbend.sc2").expect("Unable to read file");
 
-//    parse_sc2000(&[data[0]]);
-    let foo = parse_sc2000(&data);
-
-    match foo {
-        Ok(v) => {
-            println!("All good {}", v.1.file_size1);
-//            println!("All good {}", v.1.file_size2);
-//            println!("All good {}", v.1.file_size3);
-//            println!("All good {}", v.1.file_size4);
+    match parse_sc2000(&bin) {
+        Ok(result) => {
+            println!("File is {} bytes long", result.1.total_bytes);
+            println!(
+                "File type is {}{}{}{}",
+                result.1.file_type[0],
+                result.1.file_type[1],
+                result.1.file_type[2],
+                result.1.file_type[3]
+            );
         },
-        Err(e) => println!("All bad")
+        Err(e) => println!("Error parsing file")
     }
-
-//    println!("{}", x.file_size);
-
-//    let (input, _) = tag("FORM")(data);
-//    println!("{}", input)
-
-//    for x in data {
-//        if x.is_alphanum() {
-//            print!("{}", x.as_char());
-//        } else {
-//            print!(".")
-//        }
-//    }
 }
